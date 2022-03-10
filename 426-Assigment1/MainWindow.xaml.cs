8ae3ff705732cb3c8fa55b0ead2e4c52db0bae1c
ceren98 <cereneycan98@gmail.com>
2022-03-10 20:38:47 +0300

    encrypt

--- 426-Assigment1/MainWindow.xaml.cs
+++ 426-Assigment1/MainWindow.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Numerics;
-using System.Text.RegularExpressions;
+using System.Windows;
 
 namespace _426_Assigment1
 {
@@ -22,18 +10,71 @@ namespace _426_Assigment1
     /// </summary>
     public partial class MainWindow : Window
     {
-        BigInteger   n, z;
-        
+        BigInteger n, z, e;
+        BigInteger dPrivate;
+        string text;
+        string cipherText = "";
+        int strLength;
+        BigInteger[] numArr;
+        int num ,m;
+        string numS;
+
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        public bool PrimeChk() //checks if the number is prime or not
+        {
+            BigInteger p = int.Parse(TextBox1.Text);
+            BigInteger q = int.Parse(TextBox2.Text);
+
+            if (p <= 1 || q <= 1) return false;
+            if (p == 2 && q == 2) return true;
+            if (p == 0 || q == 0) return false;
+            int i, j;
+            for (i = 3; i <= p - 1; i++)
+            {
+                if (p % i == 0)
+                    return false;
+            }
+            if (p == i)
+                return true;
+            return true;
+
+            for (j = 3; j <= q - 1; j++)
+            {
+                if (q % j == 0)
+                    return false;
+            }
+            if (q == j)
+                return true;
+            return true;
+        }
+        public int GetN(int p, int q) //calculates n
+        {
+            int n = p * q;
+            return n;
+        }
+
+
+
+
         private void Calculation_Copy_Click(object sender, RoutedEventArgs e)
         {
             BigInteger ePrime = int.Parse(eTextBox.Text);
-            var d = ePrime % z;
-            BigInteger dPrivate = 1 / d;
+
+            int c = 1;
+            for (int i = 0; i < ePrime; i++)
+            {
+                c = (int)(c * dPrivate % n);
+                c = (int)(c % n);
+            }
+
+
+            c = (int)(ePrime % z);
+            dPrivate = 1 / c;
 
             ePublicKey1.Text = ePrime.ToString();
             nPublicKey1.Text = nTextBox.Text;
@@ -41,11 +82,13 @@ namespace _426_Assigment1
             nPrivateKey1.Text = nTextBox.Text;
         }
 
+
+
         private void Calculation_Click(object sender, RoutedEventArgs e)
         {
             BigInteger p = int.Parse(TextBox1.Text);
             BigInteger q = int.Parse(TextBox2.Text);
-            if (p%2!=0)
+            if (p % 2 != 0)
             {
                 n = p * q;
                 z = (p - 1) * (q - 1);
@@ -54,10 +97,92 @@ namespace _426_Assigment1
             {
                 return;
             }
-            
+
             nTextBox.Text = n.ToString();
             zTextBox.Text = z.ToString();
+
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            enTextBox1.Text = "";
+            enTextBox2.Text = "";
+            enTextBox3.Text = "";
+            enTextBox4.Text = "";
+            enTextBox5.Text = "";
+
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void encryptButton_Click(object sender, RoutedEventArgs e)
+        {
+            Encrypt();
+        }
+
+        private void GetValues()
+        {
+            e = int.Parse(enTextBox1.Text);
+            n = int.Parse(enTextBox2.Text);
+
+        }
+
+
+        private void Encrypt()
+        {
+
+            text = enTextBox3.Text.ToString();
+            strLength = text.Length;
+            e = int.Parse(enTextBox1.Text);
+            n = int.Parse(enTextBox2.Text);
+            int cipherInt;
+
+            
+
+            List<int> numbers = new List<int>();
+            List<string> hex = new List<string>();
+
+            char[] letters = text.ToCharArray();
+            for (int i = 0; i < letters.Length; i++)
+            {   
+               
+                cipherInt = Convert.ToInt32(letters[i]);
+                m = cipherInt;
+                int c = 1; 
+                for ( int k = 0; k < e; k++)
+                {
+                    c = c * m % (int)n;
+                    c = c % (int)n;
+                    numbers.Add(c);
+                    numS = c.ToString("X4");
+                    hex.Add(numS);
+                }
+                
+                foreach (int mynum in numbers)
+                {
+
+                    enTextBox4.Text = mynum.ToString();
+                    Console.WriteLine(mynum.ToString());
+                }
+
+                foreach(string myhex in hex)
+                {
+                    enTextBox5.Text = myhex.ToString();
+                }
+
+                //    foreach(int item in numArr)
+                {
+                //    numArr[i] += num;
+              //      enTextBox4.Text= item.ToString() + ",";
+                }
+            }
             
         }
+
+        
+
     }
 }
